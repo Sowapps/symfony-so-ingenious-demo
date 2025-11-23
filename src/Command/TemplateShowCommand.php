1509ec898ae8c26ef5a43c82779930c1b9012f6c
Florent HAZARD <f.hazard@sowapps.com>
2025-11-23 09:25:23 +0100

    ✨ Template show command

--- src/Command/TemplateShowCommand.php
+++ src/Command/TemplateShowCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use App\Service\FragmentService;
+use App\Sowapps\SoIngenious\Template;
 use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -31,9 +32,45 @@ readonly class TemplateShowCommand {
             $template->getPath(),
             'Kind : ' . $template->getKind(),
             'Version : ' . $template->getVersion(),
+            '',
+            'Properties : ' . $this->renderTemplateProperties($template),
+            '',
+            'Children : ' . $this->renderTemplateChildren($template),
         ]);
 
         return Command::SUCCESS;
+    }
+
+    protected function renderTemplateProperties(Template $template): string {
+        $properties = $template->getProperties();
+        if( !$properties ) {
+            return 'None';
+        }
+
+        return $this->renderRecursiveArray($properties);
+    }
+
+    protected function renderTemplateChildren(Template $template): string {
+        $children = $template->getChildren();
+        if( !$children ) {
+            return 'None';
+        }
+
+        return $this->renderRecursiveArray($children);
+    }
+
+    protected function renderRecursiveArray(array $array, int $depth = 0): string {
+        $indent = str_repeat("  ", $depth);
+        $output = '';
+        foreach( $array as $key => $value ) {
+            $isArray = is_array($value);
+            $output .= sprintf('%s%s => %s' . "\n", $indent, $key, $isArray ? '' : $value);
+            if( $isArray ) {
+                $output .= $this->renderRecursiveArray($value, $depth + 1);
+            }
+        }
+
+        return $output;
     }
 
 }
